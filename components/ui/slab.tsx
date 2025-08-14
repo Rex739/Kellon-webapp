297c290d1963a48ef97b57b6eb78afc1e64ff569
Rex739 <ojemehprogress@gmail.com>
2025-08-14 22:02:09 +0100

    feat(ui): add Slab component to indicate active navigation link

---
+++ components/ui/slab.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { FC, HtmlHTMLAttributes } from "react"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+import { isActive } from "@/lib/isActiveLink"
+
+interface CustomUiSlabProps extends HtmlHTMLAttributes<HTMLSpanElement> {
+  href: string
+}
+
+const Slab: FC<CustomUiSlabProps> = ({ className, href }) => {
+  const pathname = usePathname()
+
+  return (
+    <span
+      className={cn(
+        "flex h-1 w-20 bg-primary-50 rounded-b-sm invisible",
+        className,
+        isActive(href, pathname) && "visible"
+      )}
+    ></span>
+  )
+}
+
+export default Slab
