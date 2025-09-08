fc4a0b27fbf669a05f1d3b64b2a2bf015323d552
Rex739 <ojemehprogress@gmail.com>
2025-09-08 10:02:49 +0100

    style(ui): enhance button variants with cursor-pointer, blue style, and full size

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive ease-in-out duration-300",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive ease-in-out duration-300 cursor-pointer",
   {
     variants: {
       variant: {
@@ -21,12 +21,15 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        blue: "bg-blue-500 hoover:bg-seconday-70 text-primary-foreground shadow-xs",
       },
+
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         xs: "h-6 rounded-sm gap-1 px-2 has-[>svg]:px-1.5",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        full: "h-10 w-full rounded-md px-6 has-[>svg]:px-6",
         icon: "size-9",
       },
     },
