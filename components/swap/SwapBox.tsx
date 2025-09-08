122d555bda052d1e17214b0a985824fd75f6b693
Rex739 <ojemehprogress@gmail.com>
2025-09-08 09:56:18 +0100

    refactor(swap): add reuseable tokenWithChainLogo component

--- components/swap/SwapBox.tsx
+++ components/swap/SwapBox.tsx
@@ -2,8 +2,8 @@
 
 import { type ExtendedChain, type Token } from "@lifi/sdk"
 import { FC } from "react"
-import Image from "next/image"
 import { Label } from "@/components/ui/label"
+import TokenWithChainLogo from "./TokenWithChainLogo"
 
 interface SwapBoxProps {
   chains: ExtendedChain[]
@@ -29,7 +29,9 @@ const SwapBox: FC<SwapBoxProps> = ({
         onClick={() => handleChainSelectOpen("from")}
         className="bg-white2 dark:bg-secondary-60 rounded-lg flex flex-col p-3 space-y-4 border border-input cursor-pointer"
       >
-        <Label className="text-sm text-black dark:text-white font-semibold">From</Label>
+        <Label className="text-sm text-black dark:text-white font-semibold">
+          From
+        </Label>
 
         {/* Show selected chain + token if available */}
         <div>
@@ -37,43 +39,11 @@ const SwapBox: FC<SwapBoxProps> = ({
             <div className="flex space-x-4">
               {/* Token + chain logos */}
               <div className="flex items-center space-x-2">
-                <div className="w-11 h-11 bg-white1 dark:bg-secondary-70 rounded-full relative">
-                  {/* Token logo or fallback */}
-                  {fromToken.logoURI ? (
-                    <Image
-                      src={fromToken.logoURI}
-                      alt={fromToken.symbol}
-                      width={100}
-                      height={100}
-                      className="rounded-full"
-                    />
-                  ) : (
-                    <div className="rounded-full flex justify-center items-center bg-blue-950 text-white p-2 w-11 h-11">
-                      {fromToken.name?.charAt(0).toUpperCase()}
-                    </div>
-                  )}
-
-                  {/* Chain logo overlay or fallback */}
-                  {chains.find((c) => c.id === fromChain) ? (
-                    <div className="w-5 h-5 border-2 rounded-full border-white2 dark:border-secondary-60 absolute -bottom-1.5 right-0">
-                      <Image
-                        src={
-                          chains.find((c) => c.id === fromChain)?.logoURI ??
-                          "/placeholder.png"
-                        }
-                        alt={
-                          chains.find((c) => c.id === fromChain)?.name ||
-                          "Chain"
-                        }
-                        width={100}
-                        height={100}
-                        className="rounded-full"
-                      />
-                    </div>
-                  ) : (
-                    <div className="w-5 h-5 bg-white1 dark:bg-secondary-70 rounded-full border-white2 dark:border-secondary-60 border-2 absolute -bottom-1.5 right-0" />
-                  )}
-                </div>
+                <TokenWithChainLogo
+                  token={fromToken}
+                  chains={chains}
+                  chainId={fromChain}
+                />
               </div>
 
               {/* Token + chain text */}
@@ -105,7 +75,9 @@ const SwapBox: FC<SwapBoxProps> = ({
         onClick={() => handleChainSelectOpen("to")}
         className="bg-white2 dark:bg-secondary-60 rounded-lg flex flex-col p-3 space-y-4 border border-input cursor-pointer"
       >
-        <Label className="text-sm text-black dark:text-white font-semibold">To</Label>
+        <Label className="text-sm text-black dark:text-white font-semibold">
+          To
+        </Label>
 
         {/* Show selected chain + token if available */}
         <div>
@@ -113,42 +85,11 @@ const SwapBox: FC<SwapBoxProps> = ({
             <div className="flex space-x-4">
               {/* Token + chain logos */}
               <div className="flex items-center space-x-2">
-                <div className="w-11 h-11 bg-white1 dark:bg-secondary-70 rounded-full relative">
-                  {/* Token logo or fallback */}
-                  {toToken.logoURI ? (
-                    <Image
-                      src={toToken.logoURI}
-                      alt={toToken.symbol}
-                      width={100}
-                      height={100}
-                      className="rounded-full"
-                    />
-                  ) : (
-                    <div className="rounded-full flex justify-center items-center bg-blue-950 text-white p-2 w-11 h-11">
-                      {toToken.name?.charAt(0).toUpperCase()}
-                    </div>
-                  )}
-
-                  {/* Chain logo overlay or fallback */}
-                  {chains.find((c) => c.id === toChain) ? (
-                    <div className="w-5 h-5 border-2 rounded-full border-white2 dark:border-secondary-60 absolute -bottom-1.5 right-0">
-                      <Image
-                        src={
-                          chains.find((c) => c.id === toChain)?.logoURI ??
-                          "/placeholder.png"
-                        }
-                        alt={
-                          chains.find((c) => c.id === toChain)?.name || "Chain"
-                        }
-                        width={100}
-                        height={100}
-                        className="rounded-full"
-                      />
-                    </div>
-                  ) : (
-                    <div className="w-5 h-5 bg-white1 dark:bg-secondary-70 rounded-full border-white2 dark:border-secondary-60 border-2 absolute -bottom-1.5 right-0" />
-                  )}
-                </div>
+                <TokenWithChainLogo
+                  token={toToken}
+                  chains={chains}
+                  chainId={toChain}
+                />
               </div>
 
               {/* Token + chain text */}
