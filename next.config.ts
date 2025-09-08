b464221911b661f9bbc8fa0b9041f76d078aebdd
Rex739 <ojemehprogress@gmail.com>
2025-09-08 10:06:50 +0100

    chore(config): extend Next.js image remotePatterns with multiple external hosts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,192 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.debank.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "anzen.finance",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.gravity.xyz",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s2.coinmarketcap.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "etherscan.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "arbiscan.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "strapi.jumper.exchange",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "polygonscan.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fraction.fyi",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tokenlist.superfluid.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "superfluid-finance.github.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "snowtrace.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.bonsai.meme",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "optimistic.etherscan.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "minerva.digital",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.towns.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "basescan.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "monerium.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ftmscan.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "icodrops.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sei.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "niftyleague.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.prod.website-files.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.sonex.so",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.reservoir.tools",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "benybadboy.b-cdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "hub.berachain.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.geckoterminal.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "%20",
+        pathname: "/**",
+      },
+    ],
+  },
+}
+
+
 
 export default nextConfig;
