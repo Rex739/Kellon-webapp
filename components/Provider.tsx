4f17d274244fbf21bede4f5ee8c22ae9e0a7c22d
Rex739 <ojemehprogress@gmail.com>
2025-09-13 04:57:43 +0100

    refactor(ui): add relative to parent component

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -36,7 +36,7 @@ const Provider: FC<ProviderProps> = async ({ children }) => {
       >
         {/* // IMP START - SSR */}
         <Web3AuthenticationProvider web3authInitialState={web3authInitialState}>
-          <main className="font-manrope bg-gray-80 dark:bg-secondary-50 text-cryptoNight dark:text-white ">
+          <main className="font-manrope bg-gray-80 dark:bg-secondary-50 text-cryptoNight dark:text-white relative">
             {children}
           </main>
         </Web3AuthenticationProvider>
