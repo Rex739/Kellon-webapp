6edcbf8b786e3584700d5b7bc5740b64f0624cec
Rex739 <ojemehprogress@gmail.com>
2026-04-06 22:36:13 +0100

    feat(wallet): create route for the authentication 'continue' page

---
+++ app/(auth)/(routes)/continue/page.tsx
@@ -0,0 +1,13 @@
+import Continue from "@/components/auth/Continue"
+import { FC } from "react"
+
+
+const page: FC = ({}) => {
+  return (
+    <div>
+      <Continue />
+    </div>
+  )
+}
+
+export default page
