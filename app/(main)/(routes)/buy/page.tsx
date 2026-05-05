82f303cc0750fff9c5ee2c20242fa2ae4daa3125
Rex739 <ojemehprogress@gmail.com>
2026-05-05 14:27:45 +0100

    feat(buy): add main buy page and mount BuyCryptoFlowManager

---
+++ app/(main)/(routes)/buy/page.tsx
@@ -0,0 +1,9 @@
+import BuyCryptoFlowManager from "@/components/wallet/buy-crypto/BuyCryptoFlowManager"
+
+export default async function BuyPage() {
+  return (
+    <main className="min-h-[100dvh]">
+      <BuyCryptoFlowManager />
+    </main>
+  )
+}
