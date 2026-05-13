11eb30ef17636b4651fc1e5215d3c23f5283280b
Rex739 <ojemehprogress@gmail.com>
2026-05-13 13:00:25 +0100

    optimize QuickAction interactions for mobile and desktop

--- components/wallet/QuickAction.tsx
+++ components/wallet/QuickAction.tsx
@@ -15,18 +15,17 @@ export default function QuickAction({
   return (
     <button
       onClick={onClick}
-      className="group flex w-full flex-col items-center outline-none md:rounded-lg md:border md:border-input text-black dark:text-white md:bg-white md:p-4 md:text-left md:shadow-sm md:transition md:hover:-translate-y-0.5 md:hover:border-gray-60 md:dark:bg-secondary-20 md:dark:hover:border-white/20"
+      className="group flex w-full flex-col items-center outline-none md:rounded-lg md:border md:border-input text-black dark:text-white md:bg-white md:p-4 md:text-left md:transition  md:active:scale-98 md:duration-300 md:hover:border-gray-60 md:dark:bg-secondary-50 md:dark:hover:border-white/20 cursor-pointer hover:bg-gray-50 dark:md:hover:bg-secondary-60/50"
     >
       <div
         className={cn(
-          // Changed w-20 to w-full to allow dynamic expansion
-          "flex h-16 w-full flex-col items-center justify-center space-y-1.5 rounded-xl border border-black/5 bg-white px-1 transition-all duration-300 transform",
+          "flex h-16 w-full flex-col items-center justify-center space-y-1.5 rounded-xl border border-black/5 bg-white px-1 transition-all duration-300 transform md:transition-none md:duration-0",
           "group-hover:shadow-lg group-active:scale-95 md:h-11 md:w-11 md:rounded-lg md:group-hover:shadow-none",
-          "dark:border-white/10 dark:bg-secondary-50  dark:group-hover:border-primary-70/40 dark:group-hover:text-primary-90",
+          "dark:border-white/10 dark:bg-secondary-50  dark:group-hover:border-primary-70/40 md:border-none md:bg-transparent dark:md:border-none dark:md:bg-transparent",
         )}
       >
         <span className="shrink-0 ">{icon}</span>
-        <span className="text-[7px] xs:text-[9px]  tracking-tight transition-colors group-hover:text-cryptoNight  dark:group-hover:text-white md:hidden">
+        <span className="text-[7px] xs:text-[9px] tracking-tight transition-colors group-hover:text-cryptoNight  dark:group-hover:text-white md:hidden">
           {label}
         </span>
       </div>
